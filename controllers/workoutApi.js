765d45c2c4d2c4e2fe4bf3ff24334411544b0ad5
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-26 20:21:25 -0700

    get all workouts

--- controllers/workoutApi.js
+++ controllers/workoutApi.js
@@ -19,4 +19,14 @@ router.post ("/workouts", ({ body }, res) =>{
     });
 });
 
-//
+//get workouts 
+router.get("/workouts", async (req,res) =>{
+    try{
+        const finalWorkout = await db.Workout.aggregate([
+            {$addFields: {totalDuration: {$sum:`$exercises.duration`}
+        }}]);
+            res.json(finalWorkout);
+    }catch(err) {
+        res.json(err);
+    };
+});
